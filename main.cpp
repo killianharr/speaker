5e1a6be8c1103fe4e3eeacd6d3c3554e67b9d74d
killianharr <killianh@live.ie>
2023-04-22 12:06:00 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 DigitalIn fire(p14);
 PwmOut spkr(p26);
 AnalogIn pot1(p19);
-int main() {
+float float1 = 2000, float2 = 1000, float3 = 100;
+ int main() {
  while (1) {
  for (float i=2000.0; i<10000.0; i+=100) {
  spkr.period(1.0/i);
  spkr=0.5;
  wait(0.1);
  }
+ float1 =2000;
+ float2 = 1000;
+ float3 = 100;
  spkr=0.0;
  while(pot1.read() < 0.5) {} // this uses the pot to control the program
  }
