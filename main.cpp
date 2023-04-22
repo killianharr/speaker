47358d8969d61986d3edba15ec8b9c02e74d7eaa
killianharr <killianh@live.ie>
2023-04-22 12:08:23 +0000

    commit 3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ AnalogIn pot1(p19);
 float float1 = 2000, float2 = 1000, float3 = 100;
  int main() {
  while (1) {
- for (float i=2000.0; i<10000.0; i+=100) {
+ for (float i=float1 ; i<float2 ; i+=float3) {
  spkr.period(1.0/i);
  spkr=0.5;
  wait(0.1);
